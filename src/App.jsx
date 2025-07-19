d5fd7c17e668470d415ce16e6af6f822b85d346a
Midengrows1 <midengrows@outlook.com>
2025-07-19 16:26:08 +0500

    fulfill database and some configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const Div = styled.div`
   text-align: center;
 `
-function App() {
+function App() {   
 
   return (
     <>
+    <FontAwesomeIcon icon={faCoffee}/>
       <Div>123</Div>
     </>
   )
